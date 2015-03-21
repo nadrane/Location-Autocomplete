1be7dd264bf373902f99e9dd458d7a5ee0c28d8d
Nicholas Drane <nicholasdrane@gmail.com>
2015-03-21 15:57:13 -0500

    Minor name changes and README updates

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ http://www.unitedstateszipcodes.org/zip-code-database/ and turn it into a JSON f
 a list of lists organized as follows: [city, state, estimated population]. Estimated population
 is included so that these city/state combinations can be sorted in order of descending
 population (this makes a lot more sense than alphabetical sorting when you think about it).
+
+The script can easily be extended to create a JSON file that calculates the distances between
+city/state or zip code combinations; that is, when given a location, the script determines all other
+locations that are within x number of miles of an inputted location. This file could then be used
+to customize search results such that they are custom sorted for each user based on how far the
+result is from the user's location.
--- zip_codes.py
+++ zip_codes.py
@@ -3,7 +3,13 @@ field that supports autocomplete. The script will read a CSV file that can be do
 http://www.unitedstateszipcodes.org/zip-code-database/ and turn it into a JSON file containing
 a list of lists organized as follows: [city, state, estimated population]. Estimated population
 is included so that these city/state combinations can be sorted in order of descending
-population (this makes a lot more sense than alphabetical sorting when you think about it)."""
+population (this makes a lot more sense than alphabetical sorting when you think about it).
+
+The script can easily be extended to create a JSON file that calculates the distances between
+city/state or zip code combinations; that is, when given a location, the script determines all other
+locations that are within x number of miles of an inputted location. This file could then be used
+to customize search results such that they are custom sorted for each user based on how far the
+result is from the user's location."""
 
 # Standard Imports
 import csv
